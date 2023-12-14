06704ca679461885471d707a30c93900891a982d
Christian <ferchris82@hotmail.com>
2023-12-14 12:31:49 -0500

    Versión 0.1

--- src/org/chrisferdev/poointerfaces/imprenta/EjemploImprenta.java
+++ src/org/chrisferdev/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,23 @@
 package org.chrisferdev.poointerfaces.imprenta;
 
+import org.chrisferdev.poointerfaces.imprenta.modelo.*;
+
 public class EjemploImprenta {
     public static void main(String[] args) {
 
+        Curriculum cv =new Curriculum("John Doe", "Ingeniero de sistemas", "Resumen laboral...");
+        cv.addExperiencias("Java");
+        cv.addExperiencias("Oracle DBA");
+        cv.addExperiencias("Spring Framework");
+        cv.addExperiencias("Desarrollador fullstack");
+        cv.addExperiencias("Angular");
+
+        Informe informe = new Informe("Martin Fowler", "James", "Estudio sobre microservicios");
+        imprimir(cv);
+        imprimir(informe);
+    }
+
+    public static void imprimir(Hoja imprimible){
+        System.out.println(imprimible.imprimir());
     }
 }
